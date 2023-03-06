2ae0a90be287d4889da3c77f6fa669492a2f2169
kot-vmeshke <81980041+kot-vmeshke@users.noreply.github.com>
2023-03-06 18:02:44 +0100

    Update modules.js

--- scripts/modules.js
+++ scripts/modules.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
     };
   });
   for (let module of dataModules) {
-    console.log(module);
     if (module.title !== 'Дашборд') {
       let xhr = new XMLHttpRequest();
       xhr.open('GET', module.href, false);
@@ -17,26 +16,37 @@ document.addEventListener('DOMContentLoaded', () => {
       let html = document.createElement('div');
       html.innerHTML = xhr.responseText;
 
-      let scales = html.querySelectorAll('#module-progress tbody tr');
-      let allLessons;
-      let userLessons;
-      let scaleName = 'Модуль';
-      scales.forEach((item) => {
-        if (item.querySelector('a').innerText.includes(scaleName)) {
-          let allLessonsStr = item.querySelector('a').innerText.split('/');
-          allLessons = allLessonsStr[allLessonsStr.length - 1];
-          userLessons = item.querySelector('.badge').innerText.split(' ')[0];
-        }
-      });
+      let allLessons = '0';
+      let userLessons = '0';
+
       let newModule = document.createElement('a');
       newModule.href = module.href;
       newModule.classList.add('custom-module');
       newModule.innerHTML = `
         <span class="custom-module__number">Модуль ${module.number}</span>
         <span class="custom-module__title">${module.title}</span>
-        <span class="custom-module__done"><b>${userLessons}</b>&nbsp;/ ${allLessons}</span>
+        <span class="custom-module__done"></span>
         <span class="custom-module__button">Перейти</span>
       `;
+
+      let scales = html.querySelectorAll('#module-progress tbody tr');
+      let scaleName = 'Модуль';
+
+      if (scales.length >= 1) {
+        scales.forEach((item) => {
+          if (item.querySelector('a').innerText.includes(scaleName)) {
+            let allLessonsStr = item.querySelector('a').innerText.split('/');
+            allLessons = allLessonsStr[allLessonsStr.length - 1];
+            userLessons = item.querySelector('.badge').innerText.split(' ')[0];
+            newModule.querySelector(
+              '.custom-module__done'
+            ).innerHTML = `<b>${userLessons}</b>&nbsp;/ ${allLessons}`;
+          }
+        });
+      } else {
+        newModule.querySelector('.custom-module__done').style.display = 'none';
+      }
+
       modulesCustom.append(newModule);
     }
   }
