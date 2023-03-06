f2595719c80c669da064735adde4da3788b326cf
kot-vmeshke <81980041+kot-vmeshke@users.noreply.github.com>
2023-03-06 10:13:43 +0100

    Create modules-2.js

---
+++ scripts/modules-2.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const modulesGC = document.querySelectorAll(".stream-table tr");
+  const modulesCustom = document.querySelector(".custom-modules-wrap");
+
+  const lessonClassDone = 'user-state-accomplished';
+  const lessonClassAll = ''; // .user-state-need_accomplish
+
+  let dataModules = Array.from(modulesGC).map((item, index) => {
+    return {
+      title: item.querySelector(".stream-title").innerText,
+      number: index + 1,
+      href: item.querySelector("a").href
+    };
+  });
+  for (let module of dataModules) {
+    console.log(module);
+    if (module.title !== "Дашборд") {
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", module.href, false);
+      xhr.send();
+      let html = document.createElement("div");
+      html.innerHTML = xhr.responseText;
+
+      let allLessons = html.querySelectorAll(`.lesson-list li:not(.divider)${lessonClassAll}`).length;
+      let userLessons = Array.from(allLessons).filter(item => item.classList.contains(lessonClass)).length;
+
+      let newModule = document.createElement("a");
+      newModule.href = module.href;
+      newModule.classList.add("custom-module");
+      newModule.innerHTML = `
+        <span class="custom-module__number">Модуль ${module.number}</span>
+        <span class="custom-module__title">${module.title}</span>
+        <span class="custom-module__done"><b>${userLessons}</b>&nbsp;/ ${allLessons}</span>
+        <span class="custom-module__button">Перейти</span>
+      `;
+      modulesCustom.append(newModule);
+    }
+  }
+});
