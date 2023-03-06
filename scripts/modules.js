a4511b648180361eb4a0bc3836f0acf6fe3f7cf5
kot-vmeshke <81980041+kot-vmeshke@users.noreply.github.com>
2023-03-06 08:41:03 +0100

    Update modules.js

--- scripts/modules.js
+++ scripts/modules.js
@@ -1,33 +1,36 @@
-window.addEventListener("load", () => {
-  const modulesGC = document.querySelectorAll(".stream-table tr");
-  const modulesCustom = document.querySelector(".custom-modules-wrap");
+document.addEventListener('DOMContentLoaded', () => {
+  const modulesGC = document.querySelectorAll('.stream-table tr');
+  const modulesCustom = document.querySelector('.custom-modules-wrap');
   let dataModules = Array.from(modulesGC).map((item, index) => {
     return {
-      title: item.querySelector(".stream-title").innerText,
+      title: item.querySelector('.stream-title').innerText,
       number: index + 1,
-      href: item.querySelector("a").href
+      href: item.querySelector('a').href,
     };
   });
   for (let module of dataModules) {
     console.log(module);
-    if (module.title !== "Дашборд") {
+    if (module.title !== 'Дашборд') {
       let xhr = new XMLHttpRequest();
-      xhr.open("GET", module.href, false);
+      xhr.open('GET', module.href, false);
       xhr.send();
-      let html = document.createElement("div");
+      let html = document.createElement('div');
       html.innerHTML = xhr.responseText;
 
-      let allLessonsStr = html
-        .querySelector("#module-progress tbody a")
-        .innerText.split("/");
-      let allLessons = allLessonsStr[allLessonsStr.length - 1];
-      let userLessons = html
-        .querySelector("#module-progress tbody .badge")
-        .innerText.split(" ")[0];
-
-      let newModule = document.createElement("a");
+      let scales = html.querySelectorAll('#module-progress tbody tr');
+      let allLessons;
+      let userLessons;
+      let scaleName = 'Модуль';
+      scales.forEach((item) => {
+        if (item.querySelector('a').innerText.includes(scaleName)) {
+          let allLessonsStr = item.querySelector('a').innerText.split('/');
+          allLessons = allLessonsStr[allLessonsStr.length - 1];
+          userLessons = item.querySelector('.badge').innerText.split(' ')[0];
+        }
+      });
+      let newModule = document.createElement('a');
       newModule.href = module.href;
-      newModule.classList.add("custom-module");
+      newModule.classList.add('custom-module');
       newModule.innerHTML = `
         <span class="custom-module__number">Модуль ${module.number}</span>
         <span class="custom-module__title">${module.title}</span>
